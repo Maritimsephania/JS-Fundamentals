9a7536cd260b66d960f6c5b0ea3452f9fbed7836
Maritimsephania <kipkoechsephania.gmail.com>
2025-05-26 20:02:34 +0300

    Added 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+//const {languages} = require('C is fun', 'Python is cool', 'JavaScript is amazing');
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
